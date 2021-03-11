6260b3d4943f03cc6c97bdab37962b558f958fc9
Marfusha-91 <71018868+Marfusha-91@users.noreply.github.com>
2021-03-11 19:40:46 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,4 @@
 module.exports = function reverse(n) {
-    let result = 0;
-    while (n) { result = result * 10 + n %  10;
-      n = Math.floor(n / 10); }
-    return result;
+     n = n + " ";
+  return n.split("").reverse().join("");
 }
